40966b65cac1ec6e3c73c13b858e1f58ddb39d7c
Eric Knight <eknight@defisolutions.com>
2020-12-15 17:22:30 -0600

    2015 day 20 golang

--- 2015/Day20/golang/day20.go
+++ 2015/Day20/golang/day20.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	// "github.com/mxschmitt/golang-combinations"
+	// "math/bits"
 )
 
 // Get all prime factors of a given number n
@@ -85,12 +87,33 @@ func ProperDivisors(n uint64) map[uint64]uint64 {
 func main() {
 	num := uint64(1)
 	val := uint64(1)
-	for val < 36000000/10 {
+	for val < 36000000 {
 		num++
-		val = SumOfProperDivisors(num) + num
-		fmt.Println(num, ": ",val)
+		val = Houses(num,10,0)
+		//fmt.Println(num, ": ",val)
 	}
+	fmt.Println(num)
+	fmt.Println(val)
 	fmt.Println(ProperDivisors(val))
+
+	fmt.Println(8)
+	fmt.Println(PrimeFactors(8))
+	fmt.Println(ProperDivisors(8))
+	fmt.Println(All(PrimeFactors(8)))
+
+	target := uint64(36000000)
+	i := uint64(1)
+	result := uint64(0)
+	for result <= target {
+		result = Houses(i,11,50)
+		i++
+	}
+	fmt.Println(i,result)
+	for i = 884521 - 10;i < 884521 + 10; i++ { 
+		fmt.Println(i,Houses(i,11,50))
+	}
+	// 36191925 too high
+	// 884521 too high
 
 	// num = uint64(1)
 	// val = uint64(1)
@@ -101,3 +124,76 @@ func main() {
 	// }
 	
 }
+
+func Houses(at uint64, per uint64, cap uint64) (presents uint64) {
+	factors := All(PrimeFactors(at))
+	for _, item := range factors {
+		//fmt.Println(presents,item,at,per)
+		if cap == 0 || item * cap > at {
+			//fmt.Println("OK")
+			presents += (item * per)
+		} 
+	}
+	return presents
+}
+
+// All returns all combinations for a given string array.
+// This is essentially a powerset of the given set except that the empty set is disregarded.
+func All(set []uint64) (subsets []uint64) {
+	length := uint(len(set))
+	submap := make(map[uint64]bool)
+	subsets = append(subsets,1)
+	// Go through all possible combinations of objects
+	// from 1 (only first object in subset) to 2^length (all objects in subset)
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		var subset uint64 = 1
+		
+		for object := uint(0); object < length; object++ {
+			// checks if object is contained in subset
+			// by checking if bit 'object' is set in subsetBits
+			if (subsetBits>>object)&1 == 1 {
+				// add object to subset
+				subset *= set[object]
+			}
+		}
+		// add subset to subsets
+		submap[subset] = true
+	}
+	for key, _ := range submap {
+		subsets = append(subsets,key)
+	}
+	return subsets
+}
+
+
+// // Combinations returns combinations of n elements for a given string array.
+// // For n < 1, it equals to All and returns all combinations.
+// func Combinations(set []string, n int) (subsets [][]string) {
+// 	length := uint(len(set))
+
+// 	if n > len(set) {
+// 		n = len(set)
+// 	}
+
+// 	// Go through all possible combinations of objects
+// 	// from 1 (only first object in subset) to 2^length (all objects in subset)
+// 	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+// 		if n > 0 && bits.OnesCount(uint(subsetBits)) != n {
+// 			continue
+// 		}
+
+// 		var subset []string
+
+// 		for object := uint(0); object < length; object++ {
+// 			// checks if object is contained in subset
+// 			// by checking if bit 'object' is set in subsetBits
+// 			if (subsetBits>>object)&1 == 1 {
+// 				// add object to subset
+// 				subset = append(subset, set[object])
+// 			}
+// 		}
+// 		// add subset to subsets
+// 		subsets = append(subsets, subset)
+// 	}
+// 	return subsets
+// }
